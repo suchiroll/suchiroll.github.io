eae9ce02d49d670b877bd2e2ac12e99e78f70200
Suchitra Natarajan <sucnat@gmail.com>
2017-07-25 16:54:05 -0400

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,19 +1,6 @@
-function toggle1() {
- if( document.getElementById("hidethis1").style.display=='none' ){
-   document.getElementById("hidethis1").style.display = 'table-row';
-     $( "#hidethis1" ).slideDown( "slow", function() {
-    // Animation complete.
-  }); // set to table-row instead of an empty string
- }else{
-   document.getElementById("hidethis1").style.display = 'none';
- }
-}
-
-function toggle2() {
- if( document.getElementById("hidethis2").style.display=='none' ){
-   document.getElementById("hidethis2").style.display = 'table-row'; // set to table-row instead of an empty string
- }else{
-   document.getElementById("hidethis2").style.display = 'none';
- }
-}
-
+$(document).ready(function(){
+    $(".card").hover(function(){
+        $(".card-img").css("height", "30%"); 
+        $(".card-content").css("height", "70%");
+        }, );
+});
