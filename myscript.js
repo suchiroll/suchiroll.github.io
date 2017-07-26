e895d3238810fabd19ba614cbb4911f03178e47e
Suchitra Natarajan <sucnat@gmail.com>
2017-07-26 15:17:29 -0400

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
     $(".card").hover(function(){
-        $(".card-img").css("height", "30%"); 
-        $(".card-content").css("height", "70%");
-        }, );
+        $(".card-img").animate({height: '30%'},"fast"); 
+        $(".card-content").animate({height: '70%'},"fast"); 
+        $(this).closest(".card").css("z-index",1);
+        $(this).animate({
+        	left: '-=55',
+        	top: '-=55',
+        	height: '380px',
+        	width: '300px'
+     	   }, "fast");
+        }, function(){
+       	    $(".card-img").animate({height: '50%'},"fast"); 
+    	    $(".card-content").animate({height: '50%'},"fast"); 
+        	$(this).closest(".card").css("z-index",0); 
+       		$(this).animate({
+       			left: '+=55',
+        		right: '+=55',
+        		height: '300px',
+        		width: '300px'
+      		  }, "fast");
+        } );
+});
+
+$(document).ready(function(){
+    $(".row-title").hover(function(){
+
+    
+
+   });
 });
